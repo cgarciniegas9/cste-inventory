00edf28f7154798e6631afbaac4c7a416cff599b
cgarciniegas9 <cgarciniegas9@misena.edu.co>
2020-04-11 22:08:19 -0500

    aplicado admin lte como plantilla

---
+++ controlador/plantilla.controlador.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class PlantillaControlador{
+
+
+    public function ctrPlantilla(){
+        include "vista/plantilla.php";
+    }
+
+
+}
--- index.php
+++ index.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola Mundo</h1>
-</body>
-</html>
+<?php
+
+require "controlador/plantilla.controlador.php";
+
+
+$ctrPlantilla = new PlantillaControlador();
+$ctrPlantilla -> ctrPlantilla();
